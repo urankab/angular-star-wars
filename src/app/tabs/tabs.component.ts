142db3bb7003b52994f44d8211443c242b41a77d
Uranka Bazarsad <urankab@gmail.com>
2021-02-11 18:09:04 -0700

    inital commit

---
+++ src/app/tabs/tabs.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { StarWarsService } from '../star-wars.service';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.css']
+})
+
+export class TabsComponent implements OnInit {
+  characters = [];
+  chosenList = 'all';
+  swService: StarWarsService
+
+  constructor(swService: StarWarsService) {
+    this.swService = swService
+  }
+
+  ngOnInit() {
+  }
+
+  onChoose(side) {
+    this.chosenList = side;
+  }
+
+  getCharacters() {
+    this.characters = this.swService.getCharacters(this.chosenList)
+    return this.characters
+  }
+
+}
